8bcd27bb45be8c59ba661c82370d49ea9d6ac765
SimingFu <70648585+SimingFu@users.noreply.github.com>
2020-11-07 02:50:17 -0600

    upload

---
+++ exercise4/Exercise4.js
@@ -0,0 +1,32 @@
+document.getElementById("Borred").onclick = function (){
+  document.getElementById('txt').style.borderColor = "red";
+}
+document.getElementById("Borgreen").onclick = function (){
+  document.getElementById('txt').style.borderColor = "green";
+
+}
+document.getElementById("Borblue").onclick = function (){
+  document.getElementById('txt').style.borderColor = "blue";
+
+}
+document.getElementById("Borwidth").onclick = function (){
+  document.getElementById('txt').style.borderWidth = "thick";
+
+}
+
+document.getElementById("Bakred").onclick = function (){
+  document.getElementById('txt').style.backgroundColor = "red";
+
+}
+document.getElementById("Bakgreen").onclick = function (){
+  document.getElementById('txt').style.backgroundColor = "green";
+
+}
+document.getElementById("Bakblue").onclick = function (){
+  document.getElementById('txt').style.backgroundColor = "blue";
+
+}
+document.getElementById("Baksize").onclick = function (){
+  document.getElementById('txt').style.backgroundSize = "10px 10px";
+
+}
